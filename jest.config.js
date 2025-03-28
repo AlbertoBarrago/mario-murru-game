aa1148c67b47b5df7926e6e161409c34f2104b8f
Alberto Barrago <albertobarrago@gmail.com>
2025-03-28 16:42:33 +0100

    Added coverage

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,14 @@
 export default {
-    // Use ES modules
     transform: {
         '^.+\\.js$': 'babel-jest',
     },
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js': '$1',
     },
-    // Test environment
     testEnvironment: 'jsdom',
-    // Test files pattern
     testMatch: ['**/tests/**/*.test.js'],
-    // Coverage reporting
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['json', 'lcov', 'text', 'clover', 'json-summary'],
-    // Other options
     verbose: true
 };
